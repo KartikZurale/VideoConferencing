42c099b7a5a95af0c6292e67b5e72975ea4c1df9
Gifting Angels <giftingangelindia@gmail.com>
2025-03-19 22:15:16 +0530

    new commit 3

---
+++ app/src/main/java/com/example/videocallingapp/DashboardActivity.java
@@ -0,0 +1,84 @@
+package com.example.videocallingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    EditText codeBox;
+    Button joinBtn, shareBtn, demoBtn;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        codeBox=findViewById(R.id.codeBox);
+        joinBtn=findViewById(R.id.joinBtn);
+        shareBtn=findViewById(R.id.shareBtn);
+
+         URL serverURL;
+         try {
+             serverURL= new URL("https://meet.jit.si");
+             JitsiMeetConferenceOptions defaultOptions=
+                     new JitsiMeetConferenceOptions.Builder()
+                             .setServerURL(serverURL)
+                             .setFeatureFlag("welcomepage.enabled",false)
+                             .build();
+             JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+         } catch (MalformedURLException e) {
+             e.printStackTrace();
+         }
+
+
+        joinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JitsiMeetConferenceOptions options=new JitsiMeetConferenceOptions.Builder()
+                        .setRoom(codeBox.getText().toString())
+                        .setFeatureFlag("welcomepage.enabled",false)
+                        .build();
+                JitsiMeetActivity.launch(DashboardActivity.this, options);
+
+            }
+        });
+
+         shareBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 String string=codeBox.getText().toString();
+                 Intent intent=new Intent();
+                 intent.setAction(intent.ACTION_SEND);
+                 intent.putExtra(Intent.EXTRA_TEXT,string);
+                 intent.setType("text/plain");
+                 startActivity(intent);
+             }
+         });
+
+         demoBtn=(Button) findViewById(R.id.demoBtn);
+         demoBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 FirebaseAuth.getInstance().signOut();
+                 startActivity(new Intent(DashboardActivity.this, LoginActivity.class));
+             }
+         });
+    }
+}
